721971260eadd1ba1f48b984564d98918357f06c
Marcos Mateus <118066370+mM-b@users.noreply.github.com>
2023-01-19 15:26:30 -0300

    Add files via upload

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,31 +1,19 @@
-#include <stdio.h>
-
-int buscaBinaria(int *vetor, int tamanho, int valor) {
-    int inicio = 0;
-    int fim = tamanho - 1;
-    int meio;
-    while (inicio <= fim) {
-        meio = (inicio + fim) / 2;
-        if (vetor[meio] == valor) {
-            return meio;
-        } else if (vetor[meio] < valor) {
-            inicio = meio + 1;
-        } else {
-            fim = meio - 1;
-        }
-    }
-    return -1;
+#include<stdio.h>
+void inverte(int vetor[], int size){
+int i,ii;
+int aux[size];
+for(i=0;i<size;i++){
+  aux[i]=vetor[i];
 }
-
-int main() {
-    int vetor[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int tamanho = sizeof(vetor) / sizeof(int);
-    int valor = 5;
-    int posicao = buscaBinaria(vetor, tamanho, valor);
-    if (posicao == -1) {
-        printf("Valor não encontrado");
-    } else {
-        printf("Valor encontrado na posição %d", posicao);
-    }
-    return 0;
+for(i=0;i<size;i++){
+vetor[i]=aux[size-1-i];
+}
+for(ii=0;ii<size;ii++){
+printf("%d\n", vetor[ii]);
+}
+}
+#include<stdio.h>
+int main(){
+int vetor[5] = {1,2,3,4,5};
+inverte (vetor, 5);
 }
