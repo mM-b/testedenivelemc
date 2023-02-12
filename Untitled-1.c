71caaeb58c2eabfbd5bfeda74f0d083feb17917c
Marcos Mateus <118066370+mM-b@users.noreply.github.com>
2023-02-12 11:10:42 -0300

    Update Untitled-1.c

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,19 +1,24 @@
-#include<stdio.h>
-void inverte(int vetor[], int size){
-int i,ii;
-int aux[size];
-for(i=0;i<size;i++){
-  aux[i]=vetor[i];
+#include <stdio.h>
+void inverte(int vetor[], int size)
+{
+    int i;
+    int aux[size];
+    for (i = 0; i < size; i++)
+    {
+        aux[i] = vetor[i];
+    }
+    for (i = 0; i < size; i++)
+    {
+        vetor[i] = aux[size - 1 - i];
+    }
+    for (i = 0; i < size; i++)
+    {
+        printf("%d\n", vetor[i]);
+    }
 }
-for(i=0;i<size;i++){
-vetor[i]=aux[size-1-i];
-}
-for(ii=0;ii<size;ii++){
-printf("%d\n", vetor[ii]);
-}
-}
-#include<stdio.h>
-int main(){
-int vetor[5] = {1,2,3,4,5};
-inverte (vetor, 5);
+int main(void)
+{
+    int vetor[5] = {1, 2, 3, 4, 5};
+    inverte(vetor, 5);
+    return 0;
 }
